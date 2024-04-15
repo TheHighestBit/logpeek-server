674bec57c01552d289cfbae2c2bd6dca99cb04ad
Joosep Orasmäe <jooseporasmae@hotmail.com>
2024-04-15 12:46:58 +0300

    Fixed bug with the terminal staying colored after application exit.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,5 +2,11 @@ use logpeek_server::run;
 
 #[tokio::main]
 async fn main() {
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        println!("\x1b[0m");
+        println!("Terminal colors reset. Press Ctrl+C again to exit.");
+    });
+    
     run().await;
 }
